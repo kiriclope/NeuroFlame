Need to fix masking EI for lr training

src/configuration.py
@@ -158,7 +158,7 @@ def init_const(model):
             model.PHI0 = multivariate_normal.sample((model.Na[0],)).T
             model.PHI0 = torch.stack(
                 (model.PHI0[0], model.PHI0[0], model.PHI0[1], model.PHI0[1])
-            ).type(model.float32)
+            ).type(model.FLOAT)
         else:
             # print('Using Francesca like low rank')
             multivariate_normal = MultivariateNormal(mean_, cov_)

src/network.py
@@ -268,7 +268,7 @@ class Network(nn.Module):
 
             # self.lr = self.lr_mask * self.lr
             self.lr = self.lr / (1.0 * self.Na[0])
-            self.lr = self.lr.clamp(min=-self.Wab_T[0, 0])
+            # self.lr = self.lr.clamp(min=-self.Wab_T[0, 0])
 
             if self.IF_STP:
                 W_stp_T = self.W_stp_T + self.lr[self.slices[0], self.slices[0]].T