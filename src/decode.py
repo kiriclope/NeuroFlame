Update to fix torch's backprop error during odr training

src/decode.py
@@ -24,7 +24,7 @@ def decode_bump_torch(signal, axis=-1):
         signal_copy = signal_copy.movedim(axis, -1)
 
     # Calculating the mean along the specified (or last) axis
-    m0 = torch.nanmean(signal_copy, dim=-1).real
+    m0 = torch.nanmean(signal_copy.real, dim=-1)
 
     length = signal_copy.shape[-1]
     dPhi = 2.0 * torch.pi / length

src/lif.py
@@ -13,6 +13,7 @@ from src.lr_utils import initLR
 import warnings
 warnings.filterwarnings("ignore")
 
+
 class LIFNetwork(nn.Module):
 
     def __init__(self, conf_name, repo_root, **kwargs):
@@ -41,7 +42,7 @@ class LIFNetwork(nn.Module):
         # Initialize low rank connectivity for training
         if self.LR_TRAIN:
             initLR(self)
-        
+
         # Reset the seed
         set_seed(0)
         clear_cache()
@@ -76,7 +77,7 @@ class LIFNetwork(nn.Module):
         del weights, weight_mat
         # take weights transpose for optim
         self.Wab_T = self.Wab_T.T
-        
+
         # if self.CON_TYPE=='sparse':
         #     self.Wab_T = self.Wab_T.to_sparse()
 
@@ -93,28 +94,28 @@ class LIFNetwork(nn.Module):
         self.W_stp_T = self.Wab_T[self.slices[0], self.slices[0]].clone() / self.Jab[0, 0]
 
     def init_ff_input(self):
-        return init_ff_input(self)        
-    
+        return init_ff_input(self)
+
     def initVolt(self, ff_input=None):
         if ff_input is None:
             if self.VERBOSE:
                 print('Generating ff input')
-            
+
             ff_input = init_ff_input(self)
         else:
             ff_input.to(self.device)
             self.N_BATCH = ff_input.shape[0]
-        
+
         rec_input = torch.randn((self.IF_NMDA+1, self.N_BATCH, self.N_NEURON),  device=self.device)
         volts = torch.zeros((self.N_BATCH, self.N_NEURON),  device=self.device)
-        
+
         # Update spikes
         spikes = (volts>=self.V_THRESH)
         volts[spikes] = self.V_REST
         spikes = spikes * 1.0
-        
+
         return volts, ff_input, rec_input, spikes
-    
+
     def scaleWeights(self):
 
         # scaling recurrent weights Jab as 1 / sqrt(Kb)
@@ -124,55 +125,55 @@ class LIFNetwork(nn.Module):
         self.Jab = torch.tensor(self.Jab,  device=self.device)
         self.Jab = self.Jab.reshape(self.N_POP, self.N_POP) * self.GAIN
         self.Jab.mul_(self.V_THRESH - self.V_REST)
-        
+
         for i_pop in range(self.N_POP):
             self.Jab[:, i_pop] = self.Jab[:, i_pop] / torch.sqrt(self.Ka[0])
             self.Jab[:, i_pop] = self.Jab[:, i_pop] / self.Ka[i_pop] / self.TAU_SYN[i_pop]
-        
+
         # scaling FF weights as sqrt(K0)
         if self.VERBOSE:
             print("Ja0", self.Ja0)
-        
+
         self.Ja0 = torch.tensor(self.Ja0,  device=self.device)
         self.Ja0.mul_(self.V_THRESH - self.V_REST)
         self.Ja0 = self.Ja0.unsqueeze(0)  # add batch dim
         self.Ja0 = self.Ja0.unsqueeze(-1) # add neural dim
-        
+
         # now inputs are scaled in init_ff_input unless live update
         if self.LIVE_FF_UPDATE:
             self.Ja0.mul_(self.M0 * torch.sqrt(self.Ka[0]))
-        
+
         # scaling ff variance as 1 / sqrt(K0)
         self.VAR_FF = torch.sqrt(torch.tensor(self.VAR_FF,  device=self.device))
         self.VAR_FF.mul_(self.M0 / torch.sqrt(self.Ka[0]))
         self.VAR_FF = self.VAR_FF.unsqueeze(0)  # add batch dim
         self.VAR_FF = self.VAR_FF.unsqueeze(-1) # add neural dim
-        
+
     def update_dynamics(self, volts, ff_input, rec_input, spikes):
         '''LIF Dynamics'''
-        
+
         # update hidden state
         hidden = spikes @ self.Wab_T
-        
+
         # update stp variables
         if self.IF_STP:
             Aux = self.stp(spikes[:, self.slices[0]])  # Aux is now u * x * rates
             hidden_stp = self.J_STP * Aux @ self.W_stp_T
             hidden[:, self.slices[0]].add_(hidden_stp)
-        
+
         # update batched EtoE
         if self.IF_BATCH_J:
             hidden[:, self.slices[0]].add_(self.Jab_batch * spikes[:, self.slices[0]] @ self.W_batch_T)
-        
+
         # update reccurent input
         if self.SYN_DYN:
             rec_input[0] = rec_input[0] * self.EXP_DT_TAU_SYN + hidden * self.DT_TAU_SYN
         else:
             rec_input[0] = hidden
-        
+
         # compute net input
         net_input = ff_input + rec_input[0]
-        
+
         if self.IF_NMDA:
             hidden = spikes[:, self.slices[0]] @ self.Wab_T[self.slices[0]]
             if self.IF_STP:
@@ -183,16 +184,16 @@ class LIFNetwork(nn.Module):
 
         # Update membrane voltage
         volts = volts * self.EXP_DT_TAU + self.DT_TAU * net_input
-        
+
         # Update spikes
         spikes = volts>=self.V_THRESH
         volts[spikes] = self.V_REST
         spikes = spikes * 1.0
-        
+
         # del hidden, net_input
-        
+
         return volts, rec_input, spikes
-    
+
     def forward(self, ff_input=None, REC_LAST_ONLY=0, RET_FF=0, RET_STP=0):
         '''
         Main method of Network class, runs networks dynamics over set of timesteps
@@ -206,24 +207,24 @@ class LIFNetwork(nn.Module):
 
         # Initialization (if  ff_input is None, ff_input is generated)
         volts, ff_input, rec_input, spikes = self.initVolt(ff_input)
-        
+
         # NEED .clone() here otherwise BAD THINGS HAPPEN
         if self.IF_BATCH_J:
             self.W_batch_T = self.Wab_T[self.slices[0], self.slices[0]].clone() / self.Jab[0, 0]
-        
+
         # Add STP
         if self.IF_STP:
             self.initSTP()
             self.x_list, self.u_list = [], []
-    
+
         if self.IF_BATCH_J or self.IF_STP:
             self.Wab_T[self.slices[0], self.slices[0]] = 0
 
         # Moving average
-        
+
         mv_volts, mv_rates, mv_ff = 0, 0, 0
         volts_list, rates_list, spikes_list, ff_list = [], [], [], []
-        
+
         # Temporal loop
         for step in range(self.N_STEPS):
 
@@ -233,14 +234,14 @@ class LIFNetwork(nn.Module):
                 volts, rec_input, spikes = self.update_dynamics(volts, ff_input + noise, rec_input, spikes)
             else:
                 volts, rec_input, spikes = self.update_dynamics(volts, ff_input[:, step], rec_input, spikes)
-                
+
             # update moving average
             mv_rates += spikes
             mv_volts += volts
-            
+
             if self.LIVE_FF_UPDATE and RET_FF:
                 mv_ff += ff_input + noise
-            
+
             # Reset moving average to start at 0
             if step == self.N_STEADY-self.N_WINDOW-1:
                 mv_volts = 0.0
@@ -252,14 +253,14 @@ class LIFNetwork(nn.Module):
                 if step % self.N_WINDOW == 0:
 
                     spikes_list.append(spikes)
-                    
+
                     if self.VERBOSE:
                         print_activity(self, step, mv_rates)
-                        
+
                     if not REC_LAST_ONLY:
                         rates_list.append(mv_rates / self.N_WINDOW)
                         volts_list.append(mv_volts / self.N_WINDOW)
-                        
+
                         if self.LIVE_FF_UPDATE and RET_FF:
                             ff_list.append(mv_ff / self.N_WINDOW)
                         if self.IF_STP and RET_STP:
@@ -272,7 +273,7 @@ class LIFNetwork(nn.Module):
         # returns last step
         rates = mv_rates
         volts = mv_volts
-        
+
         # returns full sequence
         if REC_LAST_ONLY==0:
             # Stack list on 1st dim so that output is (N_BATCH, N_STEPS, N_NEURON)
@@ -284,7 +285,7 @@ class LIFNetwork(nn.Module):
 
             spikes = torch.stack(spikes_list, dim=1)
             del spikes_list
-            
+
             if self.LIVE_FF_UPDATE and RET_FF:  # returns ff input
                 self.ff_input = torch.stack(ff_list, dim=1)
                 del ff_list
@@ -292,18 +293,18 @@ class LIFNetwork(nn.Module):
             if self.IF_STP and RET_STP:  # returns stp u and x
                 self.u_list = torch.stack(self.u_list, dim=1)
                 self.x_list = torch.stack(self.x_list, dim=1)
-        
+
         # Add Linear readout (N_BATCH, N_EVAL_WIN, 1) on last few steps
         if self.LR_TRAIN:
             y_pred = self.linear(rates[:, -self.lr_eval_win:])
             del rates
             return y_pred.squeeze(-1)
-        
+
         if self.LIVE_FF_UPDATE == 0 and RET_FF:
             self.ff_input = ff_input[..., self.slices[0]]
-        
+
         del ff_input, rec_input
-        
+
         clear_cache()
-        
+
         return rates, volts, spikes

src/network.py
@@ -144,25 +144,27 @@ class Network(nn.Module):
     def initSTP(self):
         """Creates stp model for population 0"""
 
-        if self.ODR_TRAIN or self.LR_TRAIN:
-            self.J_STP = nn.Parameter(torch.tensor(self.J_STP, device=self.device))
-        else:
-            self.J_STP = torch.tensor(self.J_STP, device=self.device) / torch.sqrt(self.Ka[0])
+        self.J_STP = torch.tensor(self.J_STP, device=self.device)
+        if self.training:
+            self.J_STP = nn.Parameter(self.J_STP)
 
         self.register_buffer('W_stp_T', torch.zeros((self.Na[0], self.Na[0]), device=self.device))
 
         # NEED .clone() here otherwise BAD THINGS HAPPEN !!!
-        self.W_stp_T = (
-            self.Wab_T[self.slices[0], self.slices[0]].clone() / self.Jab[0, 0]
-        )
+        self.W_stp_T = (self.Wab_T[self.slices[0], self.slices[0]].clone() / self.Jab[0, 0])
+
+        if self.LR_TYPE == 'full':
+            self.W_stp_T = torch.sqrt(self.Ka[0]) / self.Na[0]
 
         self.Wab_T.data[self.slices[0], self.slices[0]] = 0.0
 
         if self.TRAIN_EI:
             self.train_mask[self.slices[0], self.slices[0]] = 0.0
 
+
     def init_ff_input(self):
         return init_ff_input(self)
+
 
     def initRates(self, ff_input=None):
         if ff_input is None:
@@ -184,11 +186,7 @@ class Network(nn.Module):
         else:
             ff = ff_input[:, 0]
 
-        rates = Activation()(
-            ff + rec_input[0],
-            func_name=self.TF_TYPE,
-            thresh=self.thresh,
-        )
+        rates = Activation()(ff + rec_input[0], func_name=self.TF_TYPE, thresh=self.thresh)
 
         return rates, ff_input, rec_input
 
@@ -206,7 +204,7 @@ class Network(nn.Module):
         # update stp variables
         if self.IF_STP:
             Aux = self.stp(rates[:, self.slices[0]])  # Aux is now u * x * rates
-            hidden_stp = self.J_STP * Aux @ W_stp_T
+            hidden_stp = Aux @ W_stp_T
             hidden[:, self.slices[0]] = hidden[:, self.slices[0]] + hidden_stp
 
         # update batched EtoE
@@ -294,47 +292,37 @@ class Network(nn.Module):
 
         Wab_T = self.GAIN * self.Wab_T
         if self.IF_STP:
-            W_stp_T = self.GAIN * self.W_stp_T
+            W_stp_T = self.GAIN * self.J_STP * self.W_stp_T / torch.sqrt(self.Ka[0])
 
-        if self.training:
-            # Train Low rank vectors
+        if 1:
+            Wab_train = self.Wab_train[self.slices[0], self.slices[0]]
+
             if self.LR_TRAIN:
-                self.Wab_train = self.low_rank(self.LR_NORM, self.LR_CLAMP) / self.Na[0]
+                Wab_train = self.low_rank(self.LR_NORM, self.LR_CLAMP)
 
-            # Training
-            if self.ODR_TRAIN or self.LR_TRAIN:
-                if self.IF_STP:
-                    if self.ODR_TRAIN:
-                        W_stp_T = self.GAIN * (self.W_stp_T + self.Wab_train[self.slices[0], self.slices[0]])
-                        W_stp_T = W_stp_T / torch.sqrt(self.Ka[0])
+            if self.TRAIN_SCALE == 'all':
+                Wab_train = Wab_train / self.Na[0]
+            elif self.TRAIN_SCALE == 'weak':
+                Wab_train = Wab_train / self.Ka[0]
+            elif self.TRAIN_SCALE == 'strong':
+                Wab_train = Wab_train / torch.sqrt(self.Ka[0])
 
-                    if self.LR_TRAIN:
-                        if self.LR_TYPE == 'full':
-                            # W_stp_T = self.GAIN * (self.Wab_train[self.slices[0], self.slices[0]])
-                            W_stp_T = self.GAIN * (torch.sqrt(self.Ka[0]) / self.Na[0] + self.Wab_train[self.slices[0], self.slices[0]])
-                        elif self.LR_TYPE == 'sparse':
-                            W_stp_T = self.GAIN * self.W_stp_T * self.Wab_train[self.slices[0], self.slices[0]]
-                        elif self.LR_TYPE == 'rand_full':
-                            W_stp_T = self.GAIN * (self.W_stp_T / torch.sqrt(self.Ka[0])
-                                                   + self.Wab_train[self.slices[0], self.slices[0]]) # / torch.sqrt(self.Ka[0])
-                        elif self.LR_TYPE == 'rand_sparse':
-                            Wij = 1.0 + self.Wab_train[self.slices[0], self.slices[0]] / torch.sqrt(self.Ka[0])
-                            Wij_p = clamp_tensor(Wij, 0, self.slices)
-                            W_stp_T = self.GAIN * (self.W_stp_T * Wij_p) / torch.sqrt(self.Ka[0])
-                            # W_stp_T = self.GAIN * (self.W_stp_T * Wij_p + (1.0 - self.W_stp_T) * Wij_p) / torch.sqrt(self.Ka[0])
-                            # Wij_p = (torch.rand(self.Na[0], self.Na[0], device=self.device) <=
-                            # ((self.Ka[0] / self.Na[0]) * Wij).clamp_(min=0, max=1))
-                            # W_stp_T = self.GAIN * Wij_p / torch.sqrt(self.Ka[0])
+            if self.IF_STP:
+                W_stp_T = self.GAIN * self.J_STP * (self.W_stp_T + Wab_train) / torch.sqrt(self.Ka[0])
 
-                        if self.CLAMP:
-                            W_stp_T = clamp_tensor(W_stp_T, 0, self.slices)
+                if self.LR_TYPE == 'rand_sparse':
+                    Wij = 1.0 + Wab_train
+                    Wij_p = clamp_tensor(Wij, 0, self.slices)
+                    W_stp_T = self.GAIN * (self.W_stp_T * Wij_p) / torch.sqrt(self.Ka[0])
+                    del Wij, Wij_p
+
+                if self.CLAMP:
+                    W_stp_T = clamp_tensor(W_stp_T, 0, self.slices)
 
             if self.TRAIN_EI:
                 Wab_train = normalize_tensor(self.Wab_train, 0, self.slices, self.Na)
                 Wab_train = normalize_tensor(Wab_train, 1, self.slices, self.Na)
                 Wab_T = self.GAIN * (self.Wab_T + self.train_mask * Wab_train)
-
-                del Wab_train
 
                 if self.CLAMP:
                     # Check indices Think need some transpose
@@ -435,13 +423,12 @@ class Network(nn.Module):
 
         del ff_input, rec_input
 
-        if self.training:
-            self.Wab_T = Wab_T / self.GAIN
-            del Wab_T
+        # if self.training:
+        #     self.Wab_T = Wab_T / self.GAIN
+        #     del Wab_T
 
-            if self.IF_STP:
-                self.W_stp_T = W_stp_T / self.GAIN
-                del W_stp_T
+        # if self.IF_STP:
+        #     self.W_stp_T = W_stp_T / self.GAIN
 
         clear_cache()
 